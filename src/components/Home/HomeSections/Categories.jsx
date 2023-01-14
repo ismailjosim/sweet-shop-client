7db03cc1468901c8e5cbc2538e40d70ee82d96e2
Md. Jasim <75038630+ismailjosim@users.noreply.github.com>
2023-01-15 00:45:13 +0600

    make product responsive

--- src/components/Home/HomeSections/Categories.jsx
+++ src/components/Home/HomeSections/Categories.jsx
@@ -1,8 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import { BsFillHeartFill } from "react-icons/bs";
-import product01 from '../../../assets/images/sweet01.jpg';
-
-
 
 const Categories = () => {
     const [products, setProducts] = useState([]);
@@ -20,17 +17,17 @@ const Categories = () => {
 
 
     return (
-        <div className='grid grid-cols-4 gap-5 my-10 w-11/12 mx-auto'>
+        <div className='grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 gap-5 my-10 w-11/12 mx-auto'>
             {
                 products.map((product, idx) => {
-                    const { categoryId, cover, details, img01, img02, name } = product;
+                    const { cover, name } = product;
 
-                    return <a href="/" className="relative block overflow-hidden group">
+                    return <a key={idx} href="/" className="relative block overflow-hidden group">
                         <button className="absolute hover:bg-secondary hover:text-primary right-4 top-4 z-10 rounded-full bg-white p-1.5 transition-all duration-500">
                             <span className="sr-only">Wishlist</span>
                             <BsFillHeartFill />
                         </button>
-                        <img src={product01} alt="" className="object-cover w-full h-64 transition duration-500 group-hover:scale-105 sm:h-72" />
+                        <img src={cover} alt="" className="object-cover w-full h-64 transition duration-500 group-hover:scale-105 sm:h-72" />
                         <div className="relative p-6 bg-white border border-gray-100">
                             <span className="whitespace-nowrap bg-primary text-white px-3 py-1.5 text-xs font-medium">New</span>
                             <h3 className="mt-4 text-lg font-medium text-gray-900">{name}</h3>

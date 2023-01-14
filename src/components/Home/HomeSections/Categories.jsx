7851d0002f0ba538df901b8257df2079b40c55cf
Md. Jasim <75038630+ismailjosim@users.noreply.github.com>
2023-01-15 00:39:17 +0600

    add card

--- src/components/Home/HomeSections/Categories.jsx
+++ src/components/Home/HomeSections/Categories.jsx
@@ -1,27 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { BsFillHeartFill } from "react-icons/bs";
 import product01 from '../../../assets/images/sweet01.jpg';
 
 
 
 const Categories = () => {
+    const [products, setProducts] = useState([]);
+
+
+    useEffect(() => {
+        fetch('categories.json')
+            .then(res => res.json())
+            .then(data => setProducts(data))
+
+
+    }, [])
+
+
+
+
     return (
         <div className='grid grid-cols-4 gap-5 my-10 w-11/12 mx-auto'>
-            <a href="/" class="relative block overflow-hidden group">
-                <button class="absolute hover:bg-secondary hover:text-primary right-4 top-4 z-10 rounded-full bg-white p-1.5 transition-all duration-500">
-                    <span class="sr-only">Wishlist</span>
-                    <BsFillHeartFill />
-                </button>
-                <img src={product01} alt="" class="object-cover w-full h-64 transition duration-500 group-hover:scale-105 sm:h-72" />
-                <div class="relative p-6 bg-white border border-gray-100">
-                    <span class="whitespace-nowrap bg-primary text-white px-3 py-1.5 text-xs font-medium">New</span>
-                    <h3 class="mt-4 text-lg font-medium text-gray-900">Robot Toy</h3>
-                    <p class="mt-1.5 text-sm text-gray-700">$14.99</p>
-                    <form class="mt-4">
-                        <button class="block w-full p-4 text-sm font-medium transition bg-primary text-white rounded hover:scale-105"> Add to Cart</button>
-                    </form>
-                </div>
-            </a>
+            {
+                products.map((product, idx) => {
+                    const { categoryId, cover, details, img01, img02, name } = product;
+
+                    return <a href="/" className="relative block overflow-hidden group">
+                        <button className="absolute hover:bg-secondary hover:text-primary right-4 top-4 z-10 rounded-full bg-white p-1.5 transition-all duration-500">
+                            <span className="sr-only">Wishlist</span>
+                            <BsFillHeartFill />
+                        </button>
+                        <img src={product01} alt="" className="object-cover w-full h-64 transition duration-500 group-hover:scale-105 sm:h-72" />
+                        <div className="relative p-6 bg-white border border-gray-100">
+                            <span className="whitespace-nowrap bg-primary text-white px-3 py-1.5 text-xs font-medium">New</span>
+                            <h3 className="mt-4 text-lg font-medium text-gray-900">{name}</h3>
+                            <p className="mt-1.5 text-sm text-gray-700">$14.99</p>
+                            <form className="mt-4">
+                                <button className="block w-full p-4 text-sm font-medium transition bg-primary text-white rounded hover:scale-105"> Add to Cart</button>
+                            </form>
+                        </div>
+                    </a>
+                })
+            }
+
 
         </div>
     );

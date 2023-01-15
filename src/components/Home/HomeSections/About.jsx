0c851c839465edc5f2e9e2c233a70787693d4e90
Md. Jasim <75038630+ismailjosim@users.noreply.github.com>
2023-01-15 22:02:49 +0600

    add about section

--- src/components/Home/HomeSections/About.jsx
+++ src/components/Home/HomeSections/About.jsx
@@ -1,45 +1,28 @@
 import React from 'react';
-
+import sweetBg from '../../../assets/images/sweet12.jpg'
+import img from '../../../assets/images/sweet08.jpg'
+import { AiOutlineArrowRight } from "react-icons/ai";
 const About = () => {
     return (
-        <section className="overflow-hidden bg-[url(https://images.unsplash.com/photo-1602810319428-019690571b5b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80)] bg-cover bg-center bg-no-repeat"
-        >
-            <div className="bg-black/25 p-8 md:p-12 lg:px-16 lg:py-24">
-                <div className="max-w-lg text-center sm:text-left">
-                    <h2 className="text-2xl font-bold text-white sm:text-3xl md:text-5xl">
-                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quo!
-                    </h2>
-
-                    <p
-                        className="hidden max-w-md text-white/90 md:mt-6 md:block md:text-lg md:leading-relaxed"
-                    >
-                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore
-                        officia corporis quasi doloribus iure architecto quae voluptatum beatae
-                        excepturi dolores.
-                    </p>
-
-                    <div className="mt-4 sm:mt-8">
-                        <a
-                            href="#"
-                            className="inline-flex items-center rounded-full bg-indigo-700 px-8 py-3 text-white shadow-lg transition hover:bg-indigo-600 focus:outline-none focus:ring"
-                        >
-                            <span className="text-sm font-medium"> Order Now </span>
-
-                            <svg
-                                className="ml-3 h-5 w-5"
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                stroke="currentColor"
-                            >
-                                <path
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
-                                    stroke-width="2"
-                                    d="M17 8l4 4m0 0l-4 4m4-4H3"
-                                />
-                            </svg>
-                        </a>
+        <section style={{ backgroundImage: `url(${ sweetBg })` }} className="overflow-hidden bg-cover bg-center bg-no-repeat">
+            <div class="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 sm:py-24 lg:px-8">
+                <div class="mt-8 grid grid-cols-1 gap-8 lg:grid-cols-2 lg:gap-16">
+                    <div class="relative h-64 overflow-hidden sm:h-80 lg:h-full">
+                        <img alt="" src={img} class="absolute inset-0 h-full w-full object-cover" />
+                    </div>
+                    <div className="p-8 md:p-12 lg:px-16 lg:py-24">
+                        <div className="max-w-lg text-center sm:text-left">
+                            <h2 className="text-2xl font-bold text-white sm:text-3xl md:text-5xl">About Us</h2>
+                            <p className="max-w-md text-white/90 md:mt-6 md:block md:text-lg md:leading-relaxed">
+                                After serving so many customers, today we can proudly say that our shop, JS Design, situated at , is home to a wide range of sweets that cater to the diverse taste buds of every patron. All of our delicacies are made using unique recipes that will leave a memorable aftertaste in your mouth. You can call for sweets in times of sudden sweet cravings or when you wish to complete a meal with a delightful dessert.
+                            </p>
+                            <div className="mt-4 sm:mt-8">
+                                <button className="btn btn-primary rounded-none text-white flex gap-1 font-bold" >
+                                    <span className="text-sm font-medium">Learn More</span>
+                                    <AiOutlineArrowRight />
+                                </button>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -49,3 +32,5 @@ const About = () => {
 };
 
 export default About;
+
+
